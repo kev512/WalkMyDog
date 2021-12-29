39ffa88ff50601673a68ae1e989ebd5693d5eba3
Wojciech <wojteknab@gmail.com>
2021-12-29 16:37:59 +0100

    Create dogInfo and announcementInfo classes

---
+++ app/src/main/java/com/walkmydog/data/DogInfo.kt
@@ -0,0 +1,15 @@
+package com.walkmydog.data
+
+data class DogInfo (
+    val userId: String,
+    val name: String,
+    val age: Int,
+    val weight: Float,
+    val petBehaviorQuestionFirst: Boolean,
+    val petBehaviorQuestionSecond: Boolean,
+    val petBehaviorQuestionThird: Boolean,
+    val petBehaviorQuestionFourth: Boolean,
+    val petBehaviorQuestionFifth: Boolean,
+    val petBehaviorQuestionSixth: Boolean,
+    val petBehaviorQuestionSeventh: Boolean
+)
