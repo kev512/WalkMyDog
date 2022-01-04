bb4495bdf2051af47df85f777fbe73786f7e3d1c
Wojciech <wojteknab@gmail.com>
2022-01-04 14:13:30 +0100

    create registration in RegisterActivity.kt add internet permission in manifest

--- app/src/main/java/com/walkmydog/authentication/RegisterActivity.kt
+++ app/src/main/java/com/walkmydog/authentication/RegisterActivity.kt
@@ -1,20 +1,31 @@
 package com.walkmydog.authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.walkmydog.MainActivity
 import com.walkmydog.R
+import org.jetbrains.annotations.NotNull
 
 class RegisterActivity : AppCompatActivity() {
 
-   lateinit var mPersonNameRegister: EditText
-   lateinit var mEmailRegister: EditText
-   lateinit var mPasswordRegister: EditText
-   lateinit var mPhoneNumberRegister: EditText
-   lateinit var mLoginBtnActivity: TextView
-   lateinit var mRegisterBtn: Button
+   private lateinit var mPersonNameRegister: EditText
+   private lateinit var mEmailRegister: EditText
+   private lateinit var mPasswordRegister: EditText
+   private lateinit var mPhoneNumberRegister: EditText
+   private lateinit var mLoginBtnActivity: TextView
+   private lateinit var mRegisterBtn: Button
+   private lateinit var fAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +37,46 @@ class RegisterActivity : AppCompatActivity() {
         mPhoneNumberRegister = findViewById(R.id.editTextPhoneRegister)
         mLoginBtnActivity = findViewById(R.id.loginTextViewInRegisterActivity)
         mRegisterBtn = findViewById(R.id.registerButton)
+
+        fAuth = FirebaseAuth.getInstance()
+
+        //check auth
+        if(fAuth.currentUser != null){
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+        }
+
+        mRegisterBtn.setOnClickListener(View.OnClickListener {
+            fun onClick() {
+                val email: String = mEmailRegister.text.toString().trim()
+                val password: String = mPasswordRegister.text.toString().trim()
+
+                if(TextUtils.isEmpty(email)){
+                    mEmailRegister.setError("Email is required !")
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPasswordRegister.setError("Password is required !")
+                }
+
+                if(password.length < 8){
+                    mPasswordRegister.setError("Password must be at 8 chars or longer !")
+                }
+
+
+                //register user in Firebase
+                fAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(OnCompleteListener {
+                        fun onComplete(task: Task<AuthResult>){
+                            if(task.isSuccessful){
+                               Toast.makeText(this, "User Created.", Toast.LENGTH_SHORT).show()
+                               startActivity(Intent(applicationContext, MainActivity::class.java))
+                            }else {
+                                Toast.makeText(this, "Error ! " + task.exception, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    })
+            }
+
+        })
     }
 }
